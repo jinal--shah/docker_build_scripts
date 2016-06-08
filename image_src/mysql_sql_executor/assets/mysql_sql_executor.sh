8ef28309fd27b4b2a7c5800327c1229da990fd99
jinal--shah <jnshah@gmail.com>
2016-06-08 15:04:32 +0000

    added mysql_sql_executor image - run some sql fetched from S3

---
+++ image_src/mysql_sql_executor/assets/mysql_sql_executor.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# vim: ts=4 sw=4 et sr smartindent:
+
+SQL_LOCAL=/var/tmp/my.sql
+SQL_ZIPPED=$SQL_LOCAL.gz
+MYSQL_OPTS="--host=$DBHOST --port=$DBPORT --user=$DBUSER --password=$DBPASS $DBNAME"
+REQUIRED_VARS="
+    AWS_REGION
+    DBHOST
+    DBPASS
+    DBPORT
+    DBUSER
+    SQL_S3_PATH
+"
+
+function check_var_defined() {
+    var_name="$1"
+    var_val="${!var_name}"
+    if [[ -z $var_val ]]; then
+        echo "$0 ERROR: You must pass \$$var_name to this script" >&2
+        FAILED_VALIDATION="you bet'cha"
+        return 1
+    fi
+}
+
+# ... validate required vars
+for this_var in $REQUIRED_VARS; do
+    check_var_defined $this_var
+done
+
+if [[ ! -z $FAILED_VALIDATION ]]; then
+    echo "$0 ERROR: FAILURE. One of more required vars not passed to this script." >&2
+    echo "$0 ERROR: required vars:" $REQUIRED_VARS >&2
+    exit 1
+fi
+
+# ... test can connect to specific db
+if ! mysql $MYSQL_OPTS -e 'select "1";'
+then
+    echo "$0 ERROR: couldn't connect to mysql host:port<$DBHOST:$DBPORT> db $DBNAME"
+    exit 1
+fi
+
+# ... fetch sql
+if ! aws --region $AWS_REGION s3 cp "$SQL_S3_PATH" $SQL_ZIPPED
+then
+    echo "$0 ERROR: couldn't download $SQL_S3_PATH." >&2
+    exit 1
+fi
+
+# ... decompress
+if ! gunzip $SQL_ZIPPED
+then
+    echo "$0 ERROR: couldn't gunzip local file $SQL_ZIPPED from $SQL_S3_PATH." >&2
+    exit 1
+fi
+
+# ... run
+mysql $MYSQL_OPTS < $SQL_LOCAL
+
