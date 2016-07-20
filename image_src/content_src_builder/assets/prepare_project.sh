ddfe82af325f50d8cc184c42940e5d1e720d8217
jinal--shah <jnshah@gmail.com>
2016-07-20 11:22:11 +0000

    adding npm cache dir

--- image_src/content_src_builder/assets/prepare_project.sh
+++ image_src/content_src_builder/assets/prepare_project.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-NVM_DIR=/opt/nvm/.nvm 
-NPM_VERSION=${NPM_VERSION:-0.12.7}
-export NVM_DIR
+export NVM_DIR=/opt/nvm/.nvm 
+export NPM_CACHE_DIR=${NPM_CACHE_DIR:-/opt/npm_cache}
+export NPM_VERSION=${NPM_VERSION:-0.12.7}
 
 # ... sanity check
 if [[ ! -d $NVM_DIR ]]; then
     echo "$0 ERROR: nvm is not pre-installed on mounted volume /opt."
+    exit 1
+fi
+
+if [[ ! -d $NPM_CACHE_DIR ]]; then
+    echo "$0 ERROR: you must provide a cache dir location, mounted from the host"
     exit 1
 fi
 
@@ -21,6 +26,8 @@ echo "$0 INFO: ... sourcing nvm"
 .  /opt/nvm/.nvm/nvm.sh
 echo "$0 INFO: ... installing npm"
 nvm install $NPM_VERSION
+echo "$0 INFO: ... setting cache dir to $NPM_CACHE_DIR"
+npm config set cache $NPM_CACHE_DIR
 echo "$0 INFO: ... installing npm modules"
 npm -d install || exit 1
 echo "$0 INFO: ... installing jspm npm module globally"
